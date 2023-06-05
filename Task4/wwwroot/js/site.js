9dd978c97f3cbc279509a63425cc7a8864d45653
Salt-Upon-Wounds <kiw197204@gmail.com>
2023-06-05 13:11:10 +0300

    view done

--- Task4/wwwroot/js/site.js
+++ Task4/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(".mainSwitcher").click(function () {
+    $(".Switcher").prop('checked', this.checked);
+});
+
+$(".Switcher").click(function () {
+    if (!this.checked && $(".mainSwitcher").is(":checked")) {
+        $(".mainSwitcher").prop('checked', false)
+    }
+
+})
